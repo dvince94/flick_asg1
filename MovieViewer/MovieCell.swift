51fa8ee5b7cabc68c0818df098f05ed5eb88c445
Vincent Duong <vduong94@gmail.com>
2016-01-25 21:44:36 -0500

    Prework for 1/25

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Vincent Duong on 1/25/16.
+//  Copyright © 2016 Vincent Duong. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
